4661f7a427fa65813f6b2913f7761711b35a4e77
MoulineRo <simonovandsim@gmail.com>
2023-06-11 12:14:35 +0300

    make it better

--- exchange/tests.py
+++ exchange/tests.py
@@ -2,10 +2,7 @@ import json
 import pathlib
 import unittest
 
-import pytest
 import responses
-from django.core.management import call_command
-from freezegun import freeze_time
 from exchange.exchange_provider import MinfinExchange,MonoExchange,NbuExchange, PrivatExchange,VkurseExchange
 
 root = pathlib.Path(__file__).parent
